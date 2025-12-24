bb39953226d98a92ef213266f10cd054e4d1f13b
Aparajita Singh <apara.singh09@gmail.com>
2025-12-24 16:54:48 +0000

    BigQuery updates
    Saving CSV Files instead of Parquet
    Still the vendorId column was saved as float so added casting before clustering in BQ

--- Study/data-warehouse/data-warehouse-study.sql
+++ Study/data-warehouse/data-warehouse-study.sql
@@ -7,6 +7,55 @@ LIMIT 100;
 -- Creating external table referring to gcs path
 CREATE OR REPLACE EXTERNAL TABLE `de-zc-data-warehouse.nytaxi.external_yellow_tripdata`
 OPTIONS (
-  format = 'PARQUET',
-  uris = ['gs://de-zc-aparajita-data/yellow/yellow_tripdata_2019-*.parquet', 'gs://de-zc-aparajita-data/yellow/yellow_tripdata_2020-*.parquet']
+  format = 'csv',
+  uris = ['gs://de-zc-aparajita-data/yellow/yellow_tripdata_2019-*.csv', 'gs://de-zc-aparajita-data/yellow/yellow_tripdata_2020-*.csv']
 );
+
+-- Check yellow trip data
+SELECT * FROM de-zc-data-warehouse.nytaxi.external_yellow_tripdata limit 10;
+
+-- Create a non partitioned table from external table
+CREATE OR REPLACE TABLE de-zc-data-warehouse.nytaxi.yellow_tripdata_non_partitioned AS
+SELECT CAST(VENDORID as INTEGER) as VendorID, tpep_pickup_datetime, tpep_dropoff_datetime, passenger_count, trip_distance, RatecodeID, store_and_fwd_flag, PULocationID, DOLocationID, payment_type, fare_amount, extra, mta_tax, tip_amount, tolls_amount, improvement_surcharge, total_amount, congestion_surcharge    FROM de-zc-data-warehouse.nytaxi.external_yellow_tripdata;
+-- Need to add casting as column is saved in CSV as Float
+
+-- Create a partitioned table from external table
+CREATE OR REPLACE TABLE de-zc-data-warehouse.nytaxi.yellow_tripdata_partitioned
+PARTITION BY
+  DATE(tpep_pickup_datetime) AS
+SELECT CAST(VENDORID as INTEGER) as VendorID, tpep_pickup_datetime, tpep_dropoff_datetime, passenger_count, trip_distance, RatecodeID, store_and_fwd_flag, PULocationID, DOLocationID, payment_type, fare_amount, extra, mta_tax, tip_amount, tolls_amount, improvement_surcharge, total_amount, congestion_surcharge FROM de-zc-data-warehouse.nytaxi.external_yellow_tripdata;
+
+-- Impact of partition
+-- Scanning 1.6GB of data
+SELECT DISTINCT(VendorID)
+FROM de-zc-data-warehouse.nytaxi.yellow_tripdata_non_partitioned
+WHERE DATE(tpep_pickup_datetime) BETWEEN '2019-06-01' AND '2019-06-30';
+
+-- Scanning ~106 MB of DATA
+SELECT DISTINCT(VendorID)
+FROM de-zc-data-warehouse.nytaxi.yellow_tripdata_partitioned
+WHERE DATE(tpep_pickup_datetime) BETWEEN '2019-06-01' AND '2019-06-30';
+
+-- Let's look into the partitions
+SELECT table_name, partition_id, total_rows
+FROM `nytaxi.INFORMATION_SCHEMA.PARTITIONS`
+WHERE table_name = 'yellow_tripdata_partitioned'
+ORDER BY total_rows DESC;
+
+-- Creating a partition and cluster table
+CREATE OR REPLACE TABLE de-zc-data-warehouse.nytaxi.yellow_tripdata_partitioned_clustered
+PARTITION BY DATE(tpep_pickup_datetime)
+CLUSTER BY VendorID AS
+SELECT CAST(VENDORID as INTEGER) as VendorID, tpep_pickup_datetime, tpep_dropoff_datetime, passenger_count, trip_distance, RatecodeID, store_and_fwd_flag, PULocationID, DOLocationID, payment_type, fare_amount, extra, mta_tax, tip_amount, tolls_amount, improvement_surcharge, total_amount, congestion_surcharge FROM de-zc-data-warehouse.nytaxi.external_yellow_tripdata;
+
+-- Query scans 1.1 GB
+SELECT count(*) as trips
+FROM de-zc-data-warehouse.nytaxi.yellow_tripdata_partitioned
+WHERE DATE(tpep_pickup_datetime) BETWEEN '2019-06-01' AND '2020-12-31'
+  AND VendorID=1;
+
+-- Query scans 864.5 MB
+SELECT count(*) as trips
+FROM de-zc-data-warehouse.nytaxi.yellow_tripdata_partitioned_clustered
+WHERE DATE(tpep_pickup_datetime) BETWEEN '2019-06-01' AND '2020-12-31'
+  AND VendorID=1;
--- Study/data-warehouse/web_to_gcs.py
+++ Study/data-warehouse/web_to_gcs.py
@@ -61,6 +61,6 @@ def web_to_gcs(year, service):
 
 # web_to_gcs('2019', 'green')
 # web_to_gcs('2020', 'green')
-# web_to_gcs('2019', 'yellow')
-web_to_gcs('2020', 'yellow')
+web_to_gcs('2019', 'yellow')
+# web_to_gcs('2020', 'yellow')
 
