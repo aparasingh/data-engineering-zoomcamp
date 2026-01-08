ffdd1aa7a1713f47d7a765e3afe6cfabefe8308d
Aparajita Singh <apara.singh09@gmail.com>
2026-01-08 21:37:39 +0000

    started batch processing module

--- Study/data-warehouse/web_to_gcs.py
+++ Study/data-warehouse/web_to_gcs.py
@@ -59,8 +59,8 @@ def web_to_gcs(year, service):
         print(f"GCS: {service}/{file_name}")
 
 
-# web_to_gcs('2019', 'green')
+web_to_gcs('2019', 'green')
 # web_to_gcs('2020', 'green')
-web_to_gcs('2019', 'yellow')
+# web_to_gcs('2019', 'yellow')
 # web_to_gcs('2020', 'yellow')
 
