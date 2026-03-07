017baf3c0011a6d94136e751e85a7bc2c4470178
Aparajita Singh <apara.singh09@gmail.com>
2026-03-07 18:24:06 +0000

    Running local spark cluster notes and updates

--- Study/batch-processing/10_spark_local.py
+++ Study/batch-processing/10_spark_local.py
@@ -1,8 +1,3 @@
-#!/usr/bin/env python
-# coding: utf-8
-
-# In[1]:
-
 import argparse
 
 import pyspark
@@ -22,7 +17,6 @@ input_yellow = args.input_yellow
 output = args.output
 
 spark = SparkSession.builder \
-    .master("spark://Amits-MacBook-Air.local:7077") \
     .appName('test') \
     .getOrCreate()\
 
